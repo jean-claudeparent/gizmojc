61e9b34933fbdde0b053d0fa5d68a968cea2aedd
Jean-Claude Parent <noursicqain@gmail.com>
2015-07-03 17:21:47 -0400

    Create class envir
    Class to use to setup automated testing.

---
+++ gizmojc/src/net/noursicain/gizmojc/envir.java
@@ -0,0 +1,9 @@
+package net.noursicain.gizmojc;
+
+public class envir {
+// This class contains data about the test environment
+	
+	public Boolean TestMode = false;
+	public final String  TestFilePPath = "";
+	
+}
