57ee53b97ec4bb8475b7be9e4668e7d71d0c4534
Jean-Claude Parent <noursicqain@gmail.com>
2015-07-03 17:31:05 -0400

    Add a junit test for envir
    Add a test method and reference to junit.

---
+++ gizmojc/src/net/noursicain/gizmojc/envirTest.java
@@ -0,0 +1,14 @@
+package net.noursicain.gizmojc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class envirTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
