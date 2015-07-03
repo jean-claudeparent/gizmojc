f8aec2eae2e96fe673b6d95d201a38eefe8d2860
Jean-Claude Parent <noursicqain@gmail.com>
2015-07-03 17:45:05 -0400

    First test case envirTest.

--- gizmojc/src/net/noursicain/gizmojc/envirTest.java
+++ gizmojc/src/net/noursicain/gizmojc/envirTest.java
@@ -5,10 +5,13 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class envirTest {
-
+    public envir myEnvir = new envir();
+    
 	@Test
-	public void test() {
+	public void test1() {
+		
+		assertFalse("envirTest1 The default value of TestMode must be false.", myEnvir.TestMode);
 		fail("Not yet implemented");
-	}
+	} // end test1
 
 }
