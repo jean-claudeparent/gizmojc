4d0d93668bc8221eff67f2278259bcab8778aacc
Jean-Claude Parent <noursicain@gmail.com>
2015-07-09 22:34:57 -0400

    Define ExtractURL
    Create a non working class to extract URL from texte.

---
+++ gizmojc/src/net/noursicain/gizmojc/extractURL.java
@@ -0,0 +1,22 @@
+package net.noursicain.gizmojc;
+
+public class extractURL {
+//Ptovide a class to extract URLs from text
+	public String TextToParse;
+	public String URL;
+	
+	
+	Boolean NextURLFound() {
+		
+	// If an URL is found in TextToParse place it in 
+	// URL,return true and remove the URL from TextToParse .
+	// if no URL is found set URL and TextToParse to nothing
+		
+		
+		return false;
+		
+	} // end NextURLFound
+	
+	
+} // end class
+
