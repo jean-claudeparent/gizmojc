7617ba5886a2cf1d51c4330f954f87b1ab911828
Jean-Claude Parent <noursicqain@gmail.com>
2015-07-14 16:56:43 -0400

    Create a test that fail for URLinText

--- gizmojc/src/net/noursicain/gizmojc_test/URLintextTest.java
+++ gizmojc/src/net/noursicain/gizmojc_test/URLintextTest.java
@@ -26,16 +26,29 @@ public class URLintextTest {
 	}
 
 	@Test
-	public void test() {
+	public void test1() {
 		// URLintext with no URL to find
+		
 		myURLInText.Split("Nourl to find.");
 		assertNull(myURLInText.TextBeforeURL);
 		assertNull(myURLInText.URL);
 		assertNull(myURLInText.TextAfterURL);
+				
+	} // end test1
+	
+	@Test
+	public void test2() {
+		// URLintext with no URL to find  but must cleanup properties
+		myURLInText.TextAfterURL ="Clean this up, assign null as values";
+		myURLInText.TextBeforeURL ="Clean this up, assign null as values";
+		myURLInText.URL ="Clean this up, assign null as values";
 		
 		
-		
-		
-	}
+		myURLInText.Split("Nourl to find.");
+		assertNull(myURLInText.TextBeforeURL);
+		assertNull(myURLInText.URL);
+		assertNull(myURLInText.TextAfterURL);
+				
+	} // end test2
 
 }
