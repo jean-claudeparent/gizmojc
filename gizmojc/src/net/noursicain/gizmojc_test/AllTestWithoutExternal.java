68b1890f0f10d985b78f571e88b27410cc8a9e07
Jean-Claude Parent <noursicqain@gmail.com>
2015-07-14 16:45:35 -0400

    Create the test suite ti run test without external ressources
    Test suite for test that can run with juste the project, example no
    database call,

---
+++ gizmojc/src/net/noursicain/gizmojc_test/AllTestWithoutExternal.java
@@ -0,0 +1,19 @@
+package net.noursicain.gizmojc_test;
+
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	envirTest.class,
+	extractURLTest.class,
+	URLintextTest.class
+})
+
+public class AllTestWithoutExternal {
+
+}
